461b7d658f19cd8369ebe24a9550929f1a12b16a
earbeitman <evan.arbeitman@compass.com>
2019-04-09 16:45:19 -0400

    add logic to dynamically create burger ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,10 +2,23 @@ import React, {Component}  from 'react';
 import Aux from '../../hoc/Auxilliary'
 import Burger from '../../components/Burger/Burger'
 class BurgerBuilder extends Component{
+
+    // constructor(props){
+    //     super(props)
+    //     this.state = {...}
+    // }
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 2
+        }
+    }
     render(){
         return(
             <Aux>
-                <Burger/>
+                <Burger ingredients = {this.state.ingredients}/>
                 <div>
                     Build Controlers
                 </div>
