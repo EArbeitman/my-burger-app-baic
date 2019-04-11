8fc9aefbeff8253b112735ea3ebf7d4ec206384c
earbeitman <evan.arbeitman@compass.com>
2019-04-11 15:41:50 -0400

    got scroll working so burger doesnt overflow page add order now button add purchaseable function

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -22,7 +22,22 @@ class BurgerBuilder extends Component{
             cheese: 0,
             meat: 0
         },
-        totalPrice: 4 
+        totalPrice: 4,
+        purchaseable: false
+    }
+
+    updatePurchaseState (ingredients) {
+
+        const sum = Object.keys(ingredients)
+            .map(igKey =>{
+                return ingredients[igKey];
+            })
+            .reduce((sum, el) =>{
+                return sum + el;
+            }, 0);
+        this.setState({
+            purchaseable: sum > 0
+        })
     }
     addIngredientHandler = (type) =>{
         const oldCount = this.state.ingredients[type];
@@ -38,11 +53,15 @@ class BurgerBuilder extends Component{
             totalPrice: newPrice,
             ingredients: updatedIngredients
         })
+        this.updatePurchaseState(updatedIngredients);
 
     }
 
     removeIngredientHandler = (type) =>{
         const oldCount = this.state.ingredients[type];
+        if(oldCount <= 0){
+            return;
+        }
         const updatedCount = oldCount - 1;
         const updatedIngredients = { // create clone of state
             ...this.state.ingredients
@@ -55,14 +74,25 @@ class BurgerBuilder extends Component{
             totalPrice: newPrice,
             ingredients: updatedIngredients
         })
+        this.updatePurchaseState(updatedIngredients);
+
     }
     render(){
+        const disabledInfo = {
+            ...this.state.ingredients
+        }
+        for(let key in disabledInfo){
+            disabledInfo[key] = disabledInfo[key] <= 0
+        }
         return(
             <Aux>
                 <Burger ingredients = {this.state.ingredients}/>
                 <BuildControls 
                     ingredientAdded = {this.addIngredientHandler}
                     ingredientRemoved = {this.removeIngredientHandler}
+                    disabled = {disabledInfo}
+                    price = {this.state.totalPrice}
+                    purchaseable = {this.state.purchaseable}
                 />
             </Aux>
         );
